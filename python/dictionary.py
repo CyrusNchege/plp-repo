c1c0bd145b81f7b82fda256bc81a563700b4b03b
CyrusNchege <cyruschegecc@gmail.com>
2023-01-07 23:14:58 +0300

    Add dictionary

---
+++ python/dictionary.py
@@ -0,0 +1,18 @@
+# dictionaty
+
+student = {
+    "name":"Cyrus",
+    "age":"22",
+    "gender":"male",
+    "department":"Comp Science"
+}
+#printing the whole dictionary
+print(student)
+
+#printing specific key pair
+print(student["name"])
+
+#adding anothe key pair
+student ["Specialization"]="Software Engineering"
+
+print(student)
