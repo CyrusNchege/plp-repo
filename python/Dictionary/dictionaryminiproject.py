a1fcd50a6c09858e0c0561556fd4119c0e0dd1eb
CyrusNchege <cyruschegecc@gmail.com>
2023-01-21 07:27:15 +0300

    Update my dictionary

--- python/Dictionary/dictionaryminiproject.py
+++ python/Dictionary/dictionaryminiproject.py
@@ -13,9 +13,30 @@ word=input("Enter a word: ").lower()
 suggestions =difflib.get_close_matches(word, data, n=4)
 
 def translate(x):
+    """
+    Parameters
+    ----------
+    x : str
+        word entered by user
+    Returns
+    -------
+    data [newword]:
+        if the word is in json file loaded it returns key (defination of the word)
+    data [newword]:
+        if the word given is not correct but there is a close match user is given a suggestion
+        and this returns key (defination) of the new word.
+    There is no such word or close match to it:
+        This message is returned where the word is not founf and there is no close match
+    """
+    
     if x in data:
         return (data[x])
+    elif len(suggestions) > 0:
+        print("Did you mean, " , suggestions)
+        newword =input("Enter suggested word: ")
+        return data [newword]
     else:
-        return suggestions
+        return "There is no such word or close match to it"
+
 
 print(translate(word))
