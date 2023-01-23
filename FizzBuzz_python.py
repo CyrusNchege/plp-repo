8e9d80e2974674c28d4f55742ca82255eaa20401
CyrusNchege <cyruschegecc@gmail.com>
2023-01-23 23:25:19 +0300

    Modify fizzbuzz for reusability

--- FizzBuzz_python.py
+++ FizzBuzz_python.py
@@ -1,9 +1,27 @@
-for i in range(1,101):
-    if i % 5 == 0 and i % 3 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+
+        # You can use this but this cannot be reused
+
+# for i in range(1,101):
+#     if i % 5 == 0 and i % 3 == 0:
+#         print("FizzBuzz")
+#     elif i % 3 == 0:
+#         print("Fizz")
+#     elif i % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(i)
+
+def fizz_buzz(n):
+    for i in range(1,n+1):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizz_buzz(100)
+#fizz_buzz(anyNumber)
+
