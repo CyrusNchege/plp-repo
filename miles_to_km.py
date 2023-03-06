9814c8ab93e3b0aa570bbb5ced805a536e26075c
CyrusNchege <cyruschegecc@gmail.com>
2023-03-06 12:06:55 +0300

    Add probrem solving quiz

---
+++ miles_to_km.py
@@ -0,0 +1,9 @@
+'''
+A program that takes miles inputed and convertes it to kilometers
+'''
+miles = input("Input miles to convert to km  :")
+miles = int(miles)
+
+km = miles * 1.60934
+
+print("inputed miles equals to",  km, "km")
