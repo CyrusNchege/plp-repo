626d2e494658885421e6410454a75ffa2bd8cf39
CyrusNchege <cyruschegecc@gmail.com>
2023-04-05 12:10:54 +0300

    Refactoring fizzbuzz unitest

--- testdriven/fizzbuzz.py
+++ testdriven/fizzbuzz.py
@@ -1,11 +1,28 @@
 def fizz_buzz(number):
-    """return fizz when divisible by 3
-    return buzz when divisible by 5
-    return fizzbuzz when divisible by both 3 and 5
+    """ The function prints fizz, buzz and fizzbuzz based on divisibility of 
+    3, 5 or both respectivly 
+
+    Args: number(int): takes one parameter
+
+    Returns:
+            fizz, buzz or fizzbuzz 
     """
+    
     if number % 3 == 0 and number % 5 == 0:
         return "fizzbuzz"
     elif number % 3 == 0:
         return "fizz"
     elif number % 5 == 0:
         return "buzz"
+    else:
+        return number
+# for reusability of the fuction create an object that does specific task
+print(fizz_buzz(3))
+print(fizz_buzz(5))
+print(fizz_buzz(15))
+
+print("......")
+print("......")
+
+for i in range(1,20):
+    print(fizz_buzz(i))
--- testdriven/test.py
+++ testdriven/test.py
@@ -7,18 +7,30 @@ class FizzBuzzTest(unittest.TestCase):
     Testing the fizzbuzz
     """
 
-    def test_returns_fizz_when_divisible_by_three(self):
+    def test_fizz(self):
         """
         Test return fizz when input is
         divisible by three
         """
         self.assertEqual(fizz_buzz(3), 'fizz')
 
-    def test_returns_buzz_when_divisible_by_five(self):
+    def test_buzz(self):
         """Test return buzz when input is
         divisible by five
         """
         self.assertEqual(fizz_buzz(5), 'buzz')
+    
+    def test_fizzbuzz(self):
+        """Test return fizzbuzz when input is
+        divisible by both 5 and 3
+        """
+        self.assertEqual(fizz_buzz(15), 'fizzbuzz')
+
+    def test_number(self):
+        """Test return number when not divisible
+        by either 3 or 5
+        """
+        self.assertEqual(fizz_buzz(4), 4)
 
 if __name__ == '__main__':
     unittest.main()
